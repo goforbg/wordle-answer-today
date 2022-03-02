3e64238b720ccbeb04c2125f01809f162cb36b14
Bharadwaj Giridhar <techydevil@gmail.com>
2022-03-02 12:24:14 +0530

    Adds puppeteer core instead of puppeteer for netlify deployment.

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,19 @@
 import '../styles/globals.css'
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+      <>
+        <Script
+            async
+            strategy="afterInteractive"
+            src="https://goforbg-analytics.vercel.app/tracker.js"
+            data-ackee-server="https://goforbg-analytics.vercel.app"
+            data-ackee-domain-id="0210cc2a-6c3e-4317-ad63-56669fd96c93"
+        />
+      <Component {...pageProps} />
+        </>
+        )
 }
 
 export default MyApp
--- pages/api/solution.js
+++ pages/api/solution.js
@@ -1,23 +1,30 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+const puppeteer = require('puppeteer-core');
+const chromium = require('chrome-aws-lambda');
 
-const puppeteer = require('puppeteer');
 
 export default async function handler(req, res) {
 
-    const browser = await puppeteer.launch()
+    try {
+        const browser = await puppeteer.launch({
+            executablePath: await chromium.executablePath
+        })
 
-    const page = await browser.newPage()
+        const page = await browser.newPage()
 
-    await page.goto('https://www.nytimes.com/games/wordle/', {waitUntil: 'networkidle0'})
+        await page.goto('https://www.nytimes.com/games/wordle/')
 
-    const localStorage = await page.evaluate(() => Object.assign({}, window.localStorage));
+        const localStorage = await page.evaluate(() => Object.assign({}, window.localStorage));
 
-    const state = JSON.parse(localStorage["nyt-wordle-state"])
+        const state = JSON.parse(localStorage["nyt-wordle-state"])
 
-    console.log("Solution today is ::" + state.solution)
+        console.log("Solution today is ::" + state?.solution)
 
-    res.setHeader('Cache-Control', 's-maxage=86400');
+        res.setHeader('Cache-Control', 's-maxage=86400');
 
-    return res.status(200).json({solution: state.solution})
+        return res.status(200).json({ok: true, solution: state?.solution})
+    } catch (e) {
+        return res.status(500).send({ok: true, error: e})
+    }
 
 }
